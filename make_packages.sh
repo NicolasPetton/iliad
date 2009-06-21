40a03ae9737556dc65a60541ad87f017e34dd628
Nicolas Petton <petton.nicolas@gmail.com>
2009-06-21 00:45:16 +0000

    Added a nice make_package script from Joachim Jaeckel. Thanks Joachim!

---
+++ make_packages.sh
@@ -0,0 +1,10 @@
+GST_PACK=/usr/local/bin/gst-package
+
+CURR_DIR=`pwd`
+echo "Packaging in $CURR_DIR ..." > make_packages.log
+for i in `find . -name package.xml`
+do
+    echo "packaging $i ..."
+    $GST_PACK -t ~/.st $i >> make_packages.log
+done
+
