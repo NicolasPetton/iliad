0f01f46c6383f9c6df2cf0c412d12d5fe837796d
Nicolas Petton <petton.nicolas@gmail.com>
2009-07-09 22:48:59 +0000

    UI package

--- make_packages.sh
+++ make_packages.sh
@@ -10,3 +10,16 @@ do
     $GST_PACK -t ~/.st $i >> make_packages.log
 done
 
+if [ -d More/UI ]; then
+	for i in `find More/UI/stylesheets -type f | grep -v '\.svn'`
+		do
+    		echo "linking $i to Public..."
+			ln -s $i Public/stylesheets/
+	done
+	for i in `find More/UI/images -type f | grep -v '\.svn'`
+		do
+    		echo "linking $i to Public..."
+			ln -s $i Public/images/
+	done
+fi
+
