cdc3e3d31a2074739788e817b899eb1d1fb8939c
Nicolas Petton <petton.nicolas@gmail.com>
2010-04-12 14:32:29 +0200

    Moved ilias_ie_history.html to javascripts directory for Pharo compatibility

--- Core/Public/javascripts/iliad.js
+++ Core/Public/javascripts/iliad.js
@@ -54,7 +54,7 @@ var iliad = (function() {
 		ie67 = jQuery.browser.msie && parseInt(jQuery.browser.version) < 8;
 		if(ie67) {
 			var iDoc = jQuery("<iframe id='_iliad_ie_history_iframe'" +
-				"src='/iliad_ie_history.html'" +
+				"src='/javascripts/iliad_ie_history.html'" +
 				"style='display: none'></iframe>").prependTo("body")[0];
 			var iframe = iDoc.contentWindow.document || iDoc.document;
 			if(window.location.hash) {
